43c053fa9e201dc6af21562f1d20f5ec77678190
ugur-kellecioglu <ugur.kellecioglu@outlook.com>
2021-02-11 18:57:21 +0300

    d

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded',()=>{
+    var player = document.querySelector('.player');
+    player.style.right = '500px';
     
+    player.addEventListener('keydown',()=>{
+
+    });
 
 });
